13e06955726f20d46423893d80810b533c7dd70b
Catalina Hasnas <catalinahasnas1996@gmail.com>
2021-05-03 19:14:08 +0300

    added text to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 const navigation = ['Coffee', 'Tea', 'Milk', 'Sweeteners', 'Coffee Machines'];
 const profile = ['Your Profile', 'Your orders', 'Sign out'];
@@ -6,7 +7,11 @@ const profile = ['Your Profile', 'Your orders', 'Sign out'];
 const Header = (): JSX.Element => (
     <Fragment>
         <header className="h-large">
-            <div className="h-full bg-header bg-cover bg-center backdrop-blur-xl"/>
+            <div className="h-full bg-header bg-cover bg-center flex flex-col justify-end content-center items-center">
+                <p className=" mr-8 text-6xl font-bold text-primary">All of your coffee needs</p>
+                <p className="text-4xl mr-8 text-primary my-3">in one place </p>
+                <Link to="categories/1" className="bg-secondary rounded-sm p-4 mb-28 mt-3 text-bg tracking-wide"> SHOP HERE </Link>
+            </div>
         </header>
     </Fragment>
 );
