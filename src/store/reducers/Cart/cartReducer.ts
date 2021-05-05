3357645fe57d133d9058649cd5f368747bcc6336
Catalina Hasnas <catalinahasnas1996@gmail.com>
2021-05-05 22:18:29 +0300

    updated cartReducer

--- src/store/reducers/Cart/cartReducer.ts
+++ src/store/reducers/Cart/cartReducer.ts
@@ -16,17 +16,18 @@ export const cartReducer = (state: ICartState = initialState, action: Action) : 
     switch(action.type) {
         case ActionTypes.addToCart: {
             var item = state.orderItems.find((item) => item.id === action.payload.id)
-            var orderItems = [];
+            var orderItems: IOrderItem [] = [];
             if (item) {
-                var slabaiazveno = state.orderItems.findIndex(item => item.id === action.payload.id);
-                orderItems = [...state.orderItems, action.payload];
-                state.orderItems[slabaiazveno].amount++;
+                var existingItemIndex = state.orderItems.findIndex(item => item.id === action.payload.id);
+                orderItems = [...state.orderItems];
+                orderItems[existingItemIndex].amount++;
             } else {
                 orderItems = [...state.orderItems, action.payload];
             }
-            console.log(orderItems);
+            
             localStorage.setItem('orderItems', JSON.stringify(orderItems));
             console.log(localStorage);
+            console.log({...state, orderItems: orderItems});
             return {...state, orderItems: orderItems}
         }
         default: 
