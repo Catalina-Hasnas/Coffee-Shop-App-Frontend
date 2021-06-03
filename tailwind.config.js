91a6bdff37cbcf8bc5d02ec9cad7c2f267677fdf
Catalina Hasnas <catalinahasnas1996@gmail.com>
2021-06-03 21:56:01 +0300

    fixed tailwind custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,6 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      primary: '#2b130a', 
-      secondary: '#c44c0a',
-      primaryLight: '#e9e2c3',
-      bg: '#f5f6f8'
-    },
-    fontFamily: {
-      'sans': ['Helvetica', 'ui-sans-serif', 'system-ui'],
-    },
     extend: {
       backgroundImage: theme => ({
         'header': "url('./assets/images/background-with-coffee-beans.jpg')",
@@ -20,6 +11,16 @@ module.exports = {
       'large': '42rem',
       }),
 
+      colors: theme => ({
+        "primary": '#2b130a', 
+        "secondary": '#c44c0a',
+        "primaryLight": '#e9e2c3',
+        "bg": '#f5f6f8'
+      }),
+
+      fontFamily: theme => ({
+        'sans': ['Helvetica', 'ui-sans-serif', 'system-ui'],
+      }),
     },
     
   },
