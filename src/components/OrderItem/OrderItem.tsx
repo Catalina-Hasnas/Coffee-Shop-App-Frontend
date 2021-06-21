6b3f44ed273106e9c0343f9a588176c2d62332f0
Catalina Hasnas <catalinahasnas1996@gmail.com>
2021-06-21 15:22:51 +0300

    styling changes to cart page

--- src/components/OrderItem/OrderItem.tsx
+++ src/components/OrderItem/OrderItem.tsx
@@ -48,16 +48,37 @@ const OrderItem = (props: IProductCardProps): JSX.Element => {
         })}, [amount]);
 
     return (
-        <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5 text-primary">
-            <div className="flex items-center w-2/5"> 
+        <div className="flex items-center justify-around hover:bg-gray-100 -mx-8 px-6 py-5 text-primary">
+            <div className="flex items-center w-2/6"> 
                 <div className="">
-                    <img className="w-24 h-20" src={props.image} alt={props.title} />
+                    <img  src={props.image} alt={props.title} />
                 </div>
-                <div className="flex flex-col items-center justify-between ml-4 md:scale-100">
+                <div className="flex flex-col ml-4 md:scale-100">
 
-                    <span className="font-bold md:text-lg">{props.title}</span>
+                    <span className="font-bold md:text-md w-32">{props.title}</span>
                     
-                    <button onClick={() => dispatch({ 
+                    
+                </div>
+            </div>
+
+            { amount === props.stock? <p className="text-xs hidden md:block w-1/6">There are only {props.stock} products left in stock.</p> : null }
+            
+            <div className="flex flex-col items-center md:flex-row md:justify-center w-1/6">
+
+                <input type="button" value="+" className="px-2 bg-transparent border-secondary rounded-sm border" disabled={disableButton()} onClick={() => setAmount(amount + 1)}/>
+                    
+                <input className="mx-2 my-2 md:my-0 border text-center w-8" type="text" value={amount > props.stock ? props.stock : amount} onChange={e => verifyStock(parseInt(e.target.value))} />
+
+                <input type="button" value="-" className="px-2 bg-transparent border-secondary rounded-sm border" onClick={() => setAmount(amount - 1)}/>
+                
+            </div>
+
+            <span className="text-center w-1/6 font-semibold text-sm">{toFixed(props.unitPrice)}$</span>
+            <span className="text-center w-1/6 font-semibold text-sm">{toFixed(props.totalPrice)}$</span>
+
+            <div className="w-1/6 flex justify-center items-center">
+
+                <button onClick={() => dispatch({ 
                                 type: ActionTypes.updateCart, 
                                 payload: {
                                     id: props.id, 
@@ -67,26 +88,12 @@ const OrderItem = (props: IProductCardProps): JSX.Element => {
                                     image: props.image,
                                     title: props.title} 
                                 })} 
-                            className="rounded-sm font-semibold p-2 bg-secondary text-primaryLight text-xs transform scale-90 md:scale-100">
+                            className="w-1/2 rounded-sm font-semibold p-2 bg-secondary text-primaryLight text-xs transform scale-90 md:scale-100">
                             Remove
-                    </button>
-                </div>
+                </button>
+
             </div>
 
-            { amount === props.stock? 
-                <p className="text-xs hidden md:block">There are only {props.stock} products left in stock.</p> : null }
-            
-            <div className="flex flex-col items-center md:flex-row md:justify-center w-1/5">
-
-                <input type="button" value="+" className="px-2 bg-transparent border-secondary rounded-sm border" disabled={disableButton()} onClick={() => setAmount(amount + 1)}/>
-                    
-                <input className="mx-2 my-2 md:my-0 border text-center w-8" type="text" value={amount > props.stock ? props.stock : amount} onChange={e => verifyStock(parseInt(e.target.value))} />
-
-                <input type="button" value="-" className="px-2 bg-transparent border-secondary rounded-sm border" onClick={() => setAmount(amount - 1)}/>
-                
-            </div>
-            <span className="text-center w-1/5 font-semibold text-sm">{toFixed(props.unitPrice)}$</span>
-            <span className="text-center w-1/5 font-semibold text-sm">{toFixed(props.totalPrice)}$</span>
         </div>
     );
 }
--- src/containers/Cart.tsx
+++ src/containers/Cart.tsx
@@ -68,26 +68,29 @@ const Cart = (): JSX.Element => {
                             <h1 className="font-semibold text-2xl">Shopping Cart</h1>
                             <h2 className="font-semibold text-2xl">{orderItems.length} Items</h2>
                         </div>
-                        <div className="flex mt-10 mb-5">
-                            <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">Product Details</h3>
-                            <h3 className="font-semibold text-gray-600 text-xs uppercase w-1/5 text-center">Quantity</h3>
-                            <h3 className="font-semibold text-gray-600 text-xs uppercase w-1/5 text-center">Price</h3>
-                            <h3 className="font-semibold text-gray-600 text-xs uppercase w-1/5 text-center">Total</h3>
+                        <div className="w-full">
+                            <div className="flex mt-10 mb-5 justify-between">
+                                <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/6">Product Details</h3>
+                                <h3 className="font-semibold text-gray-600 text-xs uppercase w-1/6 text-center">Quantity</h3>
+                                <h3 className="font-semibold text-gray-600 text-xs uppercase w-1/6 text-center">Price</h3>
+                                <h3 className="font-semibold text-gray-600 text-xs uppercase w-1/6 text-center">Total</h3>
+                                <div className="w-1/6"> </div>
+                            </div>
+                            {orderItems.map((item) => {
+                                            return (
+                                                <OrderItem 
+                                                    key={item.id}
+                                                    id = {item.id}
+                                                    amount ={item.amount}
+                                                    image = {item.image}
+                                                    totalPrice = {item.totalPrice}
+                                                    unitPrice = {item.unitPrice}
+                                                    title = {item.title}
+                                                    stock= {item.stock}
+                                                />
+                                            )
+                            })}
                         </div>
-                        {orderItems.map((item) => {
-                                        return (
-                                            <OrderItem 
-                                                key={item.id}
-                                                id = {item.id}
-                                                amount ={item.amount}
-                                                image = {item.image}
-                                                totalPrice = {item.totalPrice}
-                                                unitPrice = {item.unitPrice}
-                                                title = {item.title}
-                                                stock= {item.stock}
-                                            />
-                                        )
-                        })}
 
                         <Link to="/" className="flex font-semibold text-secondary text-sm mt-10">
                             <svg className="fill-current mr-2 text-secondary w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" /></svg>
